dd086f901882e4dc19d7cb5a4fb7fe39e4e106a1
Giedrekor <43093161+Giedrekor@users.noreply.github.com>
2018-09-21 09:20:29 +0300

    komandinis

---
+++ dirbame_cia/diena_5/Giedre/JSnd/main.js
@@ -0,0 +1,263 @@
+// UZDUOTIS =======ismokti f-jas 50x===============
+// Aptrasymas) sukurti 30 getVardas ir 30 setVardas(x) f-ju. Jas iskviesti eiles tvarka "maisant" poruojant pvz:
+// setVardas1("Antanas");
+// getVardas1();
+// setVardas2("Juozas");
+// getVardas2();
+// A) sukurti (gloabalu) kintamaji pvz:
+//      var vardas =  "Tomas";
+// B) sukurti 20-30 f-ju:  getVardas1(), getVardas2(), getVardas3(), ...getVardas30()
+// kurios  turi "return" ir iskvietus f-ja ji  grazina varda ir prideti skaiciu salia  pvz: "Tomas1", Antanas22, Juozas33
+// B) sukurti 20-30 f-ju:   setVardas1(name), setVardas2(name), ... setVardas30(name),...
+// kurios   turi pakeisti varda ir prideti skaiciu salia
+// C) visas f-jas iskviesti
+// pvz.:
+var vardas = "Tomas";
+// 1
+function setVardas1(name) {
+   vardas = name + "1";
+}
+setVardas1("Tomas"); // sugalvoti vis kita zodi
+
+function getVardas1() {
+  return vardas + "1";
+}
+var x = getVardas1();   // arba  console.log(  getVardas1() );
+console.log(  x );     // isveda: Tomas11
+
+// 2 ------------
+function setVardas2(name) {
+    vardas = name + "2";
+}
+setVardas2("antanas"); // sugalvoti vis kita zodi
+
+function getVardas2() {
+    return vardas + "2";
+}
+x =  getVardas2();     
+console.log(  x );  // isveda: antanas22
+
+// 3---------------------
+function setVardas3(name) {
+    vardas = name + "3";
+}
+setVardas3("juozas");            // sugalvoti vis kita zodi
+
+function getVardas3(){
+    return vardas + "3";
+}
+x =  getVardas3();
+console.log(  x );  // isveda: juozas33
+
+// 4---------------------
+function setVardas4(name) {
+    vardas = name + "4";
+}
+setVardas4("Gabija");
+
+function getVardas4() {
+    return vardas + "4";
+}
+console.log(getVardas4());
+
+// 5---------------------
+function setVardas5(name) {
+    vardas = name + "5";
+}
+setVardas5("Gabriele");
+
+function getVardas5() {
+    return vardas + "5";
+}
+console.log(getVardas5());
+
+// 6---------------------
+function setVardas6(name) {
+    vardas = name + "6";
+}
+setVardas6("Greta");
+
+function getVardas6() {
+    return vardas + "6";
+}
+console.log(getVardas6());
+
+// 7---------------------
+function setVardas7(name) {
+    vardas = name + "7";
+}
+setVardas7("Goda");
+
+function getVardas7() {
+    return vardas + "7";
+}
+console.log(getVardas7());
+
+// 8---------------------
+function setVardas8(name) {
+    vardas = name + "8";
+}
+setVardas8("Gabrielius");
+
+function getVardas8() {
+    return vardas + "8";
+}
+console.log(getVardas8());
+
+// 9---------------------
+
+function setVardas9(name) {
+    vardas = name + "9";
+}
+setVardas9("Gustas");
+
+function getVardas9() {
+    return vardas + "9";
+}
+console.log(getVardas9());
+
+// 10---------------------
+function setVardas10(name) {
+    vardas = name + "10";
+}
+setVardas10("Guste");
+
+function getVardas10() {
+    return vardas + "10";
+}
+console.log(getVardas10());
+
+// 11---------------------
+function setVardas11(name) {
+    vardas = name + "11";
+}
+setVardas11("Gintare");
+
+function getVardas11() {
+    return vardas + "11";
+}
+console.log(getVardas11());
+
+// 12---------------------
+function setVardas12(name) {
+    vardas = name + "12";
+}
+setVardas12("Gerda");
+
+function getVardas12() {
+    return vardas + "12";
+}
+console.log(getVardas12());
+
+// 13---------------------
+function setVardas13(name) {
+    vardas = name + "13";
+}
+setVardas13("Gediminas");
+
+function getVardas13() {
+    return vardas + "13";
+}
+console.log(getVardas13());
+
+// 14---------------------
+function setVardas14(name) {
+    vardas = name + "14";
+}
+setVardas14("Gabriela");
+
+function getVardas14() {
+    return vardas + "14";
+}
+console.log(getVardas14());
+
+// 15---------------------
+function  setVardas15(name) {
+    vardas = name + "15";
+}
+setVardas15("Gintaras");
+
+function getVardas15() {
+    return vardas + "15";
+}
+console.log(getVardas15());
+
+// 16---------------------
+function setVardas16(name) {
+    vardas = name + "16";
+}
+setVardas16("Grantas");
+
+function getVardas16() {
+    return vardas + "16";
+}
+console.log(getVardas16());
+
+// 17---------------------
+function setVardas17(name) {
+    vardas = name + "17";
+}
+setVardas17("Gytė");
+
+function getVardas17() {
+    return vardas + "17";
+}
+console.log(getVardas17());
+
+// 18---------------------
+function setVardas18(name) {
+    vardas = name + "18";
+}
+setVardas18("Gertrūda");
+
+function getVardas18() {
+    return vardas + "18";
+}
+console.log(getVardas18());
+
+// 19---------------------
+function setVardas19(name) {
+    vardas = name + "19";
+}
+setVardas19("Gerardas");
+
+function getVardas19() {
+    return vardas + "19";
+}
+console.log(getVardas19());
+
+// 20---------------------
+function setVardas20(name) {
+    vardas = name + "20";
+}
+setVardas20("Gražvydas");
+
+function getVardas20() {
+    return vardas + "20";
+}
+console.log(getVardas20());
+
+// 
+// 
+// 
+// Gabrielis
+// Guostė
+// Gaudrė
+// Gleb
+// Gedas
+// Gintas
+// Gintautas
+// Gretė
+// Grytė
+// Gintė
+// Girius
+// Germantas
+// Grėtė
+// Gintautė
+// Gitana
+// Gustavas
+// German
+// Gita
+// Georgij
+// Gratas
+// Germanas
