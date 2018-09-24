fbdb9087a1132d233387a4cf574098022dec1b0d
AndrIvasc <andrejus.iv1880@gmail.com>
2018-09-24 21:32:05 +0300

    js

---
+++ dirbame_cia/diena_7/Andrejus/default-tuscias-projektas2-if/main.js
@@ -0,0 +1,15 @@
+console.log(  "Labas pasaulis"  );
+// ===============================================
+
+var arPrisijunges = false;
+var vartotojoTipas = "moderator"
+var userName = "Tomas";
+
+if (arPrisijunges) {
+  console.log("Sveiki, geros jums dienos - " + userName);
+ if (vartotojoTipas == "moderator" ) {
+  console.log("Esate rimtas useris ir mes jus sveikiname - " + vartotojoTipas);
+}
+}else {
+  console.log("Esate neprisijuges");
+}
