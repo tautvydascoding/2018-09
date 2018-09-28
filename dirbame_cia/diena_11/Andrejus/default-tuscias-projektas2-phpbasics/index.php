4a772d56a44e03aa8c8dc064795a631188f11a5a
AndrIvasc <andrejus.iv1880@gmail.com>
2018-09-28 09:57:33 +0300

    php

---
+++ dirbame_cia/diena_11/Andrejus/default-tuscias-projektas2-phpbasics/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="libs/bootstrap4/css/bootstrap.css">
+
+  <!-- !!! mano CSS vissade zemiau -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+<?php echo "================================uzduotis==================================================" ?>
+<?php
+// <!--  Susikurti 2 failus:
+// preke.php
+// index.php
+// Faile index.php naudojant include('preke.php');    f-ja "iterpti" preke.php -->
+// UZDUOTIS:
+// 1: sukurti "preke.php" faila
+// h2 (antraste)
+// p (aprasymas)
+// button (Kaina) ?>
+
+<?php include("include/preke.php") ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src=jquery/jquery.js> </script> <script type="text/javascript" src="main.js"> </script>
+
+</body>
+
+</html>
