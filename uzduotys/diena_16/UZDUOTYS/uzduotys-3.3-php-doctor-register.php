191b6ea7860fe68e03fd39cb7583b38edc52f037
Taurijus <tadas.valutis@gmail.com>
2018-10-05 14:55:46 +0300

    darbai klases

--- uzduotys/diena_16/UZDUOTYS/uzduotys-3.3-php-doctor-register.php
+++ uzduotys/diena_16/UZDUOTYS/uzduotys-3.3-php-doctor-register.php
@@ -1,7 +1,7 @@
 Vartotojui paspaudus "registruotis" mygtuka - ivestus duomenis issaugoti DB.
 
-SUkurti gydytoju registracios  forma (pvz: template-registracija.php )
-Vartotojui paspaudus "registruotis" mygtuka - vartotoja perkelti i registracija.php faila
+SUkurti gydytoju registracijos  forma (pvz: registracija.php )
+Vartotojui paspaudus "registruotis" mygtuka - vartotoja perkelti i registracija.php faila (HINT: action='registracija.php')
 
 registracija.php faile:
-  panaudoti $_GET. Duomenis paimkite is $_GET ir perduokite i   createDoctor(....) f-ja, taip jums sukurs gydytoja 
+   Duomenis paimkite is $_GET ir perduokite i   createDoctor(....) f-ja, taip jums sukurs gydytoja 
