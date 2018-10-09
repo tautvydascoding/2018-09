f112b46903d8b2cd7d1ab958ed2e74674e6ae4c9
Taurijus <tadas.valutis@gmail.com>
2018-10-09 15:43:01 +0300

    darbai klases

--- uzduotys/diena_17/UZDUOTYS/uzduotys-5-PHP-class--extends-PAVELDIMUMAS-trumpa.php
+++ uzduotys/diena_17/UZDUOTYS/uzduotys-5-PHP-class--extends-PAVELDIMUMAS-trumpa.php
@@ -6,6 +6,7 @@ class Zmogus {
 }
 class Vyras  extends Zmogus {
     private $testosteronas = 6122;
+
 }
 
 // UZDUOTIS 0.
@@ -20,3 +21,7 @@ class Vyras  extends Zmogus {
 // Vyras klases viduje atspausdinti:
 // f-ja printVyroDuomenys()
 // ugi, pusrycius, turtas
+// kur gausite klaida? Kurio kintamojo neiseina atspausdinti?
+
+// UZDUOTIS 3 (atspausdinti kintamaji, kurio neleido 2 uzdavinys)
+// klases viduje (atspekite kurios) sukurti f-ja, kuri prieina prie privataus kintamojo . ir ji atspausdina
--- uzduotys/diena_17/UZDUOTYS/uzduotys-6-Pasikartojimas-For-Pagrindu-testui.php
+++ uzduotys/diena_17/UZDUOTYS/uzduotys-6-Pasikartojimas-For-Pagrindu-testui.php
@@ -1,14 +1,14 @@
 // ---- -PASIKARTOJIMAS PROGRAMAVIMO PAGRINDU TESTUI-----
 // 1) sukurti masyva : Tomas, Jurgis, Petras, Angele, Paulius
-// 1.1) atspausdinti masyva naudojant FOR cikla, While cikla ir FOREACH cikla
-// 1.4 Atspausdinti kas antra  
-// 1.4 Atspausdinti kas trecia  
-// 1.5 Atspasudinti viska nuo 2-cios (iskaitant 2-cia)
+// 1.1) atspausdinti masyva naudojant FOR cikla, While  cikla
+// 1.4 Atspausdinti kas antra  (naudojant i+=2; )
+// 1.4 Atspausdinti kas trecia
+// 1.5 Atspasudinti viska nuo 2-cios (iskaitant 2-tra)
 
 
 // 2) sukurti matrica:
 // Dviratis, 199, 3vnt
-// Paspirtukas, 100, 4vnt  
+// Paspirtukas, 100, 4vnt
 // Riedlente, 49, 12vnt
 // 2.0) atspausdinti su FOR ciklu
 // 2.1) atspausdinti su FOR ciklu
