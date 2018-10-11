a14267503201edc87873fde4c932f4c2f6537fdb
AndrIvasc <andrejus.iv1880@gmail.com>
2018-10-11 09:13:11 +0300

    project

---
+++ dirbame_cia/diena_18/Andrejus/0-php-class-abstrakti/automob.php
@@ -0,0 +1,32 @@
+<?php
+include("pagrindai.php");
+
+class Automobilis extends Pagrindai {
+
+public $liukas = true;
+
+function atidarytiDurys() {
+
+}
+
+function vazioti($x) {
+
+}
+
+function sustoti() {
+  
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+ ?>
