07dc887c84d2d5fc0b4c93a6dc30833c951d4c31
EgidijusNek <egidijusnek@gmail.com>
2018-10-17 14:02:26 +0300

    aa

--- dirbame_cia/diena_20/Egidijus/php-cont/contact_mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$toEmail = "pastaskurionenaudoju@gmail.com";
-$mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
-if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
-print "<p class='success'>Žinutė sėkmingai išsiųsta!.</p>";
-} else {
-print "<p class='Error'>Žinutės išsiųsti nepavyko.</p>";
-}
-?>
